46ad82aecdf124e9385c459594743bd7b0685fa5
Elizaveta Dmitrienko <lizadmitrienko06@gmail.com>
2026-06-30 10:53:05 +0800

    Remove: is-palindrome - submission-5

--- Data Structures & Algorithms/is-palindrome/submission-5.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-
-        new_s = ""
-
-        for item in s:
-            if item.isalnum():
-                new_s += item.lower()
-        return new_s == new_s[::-1]
