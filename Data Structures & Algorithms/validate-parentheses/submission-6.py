e9a46a2c61d53f152b3a0f4776d868d29fcc3c99
Elizaveta Dmitrienko <lizadmitrienko06@gmail.com>
2026-07-02 11:54:47 +0800

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        st = []
+        opened = ["[", "{", "("]
+        closed = ["]", "}", ")"]
+        
+        for i in range (len(s)):
+            char = s[i]
+            print(st)
+            if char in opened:
+                st.append(char)
+            else:
+                if (len(st) == 0):
+                    return False
+                if closed.index(char) == opened.index(st[-1]):
+                    st.pop()
+                else:
+                    return False
+        return len(st) == 0
+
+
+                
